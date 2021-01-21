2c7589b53ac3f68ed37e83f81849b2118bf25e75
Tim Ganter <1911000+timganter@users.noreply.github.com>
2021-01-20 21:28:52 -0800

    First commit.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function App () {
+  return (
+    <div>
+      <h1 className="font-sans text-red-800 text-4xl text-center pt-12">hello world</h1>
+    </div>
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+/// <reference path='./index.d.ts'/>
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { App } from './App'
+
+import './index.css'
+
+if (module.hot) {
+  module.hot.accept()
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
