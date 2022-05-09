6de9471d8d41c89e1c3a4e41ac3198b68e8cff4e
Aleksei Shugaev <a.shugaev@admitad.com>
2022-05-09 15:08:42 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function App () {
   return (
     <div>
-      <h1 className="font-sans text-red-800 text-4xl text-center pt-12">hello world</h1>
+
     </div>
   )
 }
